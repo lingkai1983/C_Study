f81272296f70984879c055ffaf2552110c237a67
lzuo7 <lingkai.zuo@gatech.edu>
2017-08-02 13:37:10 -0700

    update along with class

--- Exercise Files/04-05_multi2.c
+++ Exercise Files/04-05_multi2.c
@@ -2,15 +2,16 @@
 
 int main()
 {
-	char names[4][7] = {
+	char names[5][10] = {
 		"Mickey",
 		"Minnie",
 		"Goofy",
-		"Pluto"
+		"Pluto",
+		"Pinocchio"
 	};
 	int x;
 
-	for(x=0;x<4;x++)
+	for(x=0;x<5;x++)
 		printf("%s\n",names[x]);
 
 	return(0);
