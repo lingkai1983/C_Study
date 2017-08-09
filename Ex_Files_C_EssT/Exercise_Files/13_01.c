b909a67189b4218c38f62e083b968f88239353f3
lzuo7 <lingkai.zuo@gatech.edu>
2017-08-09 16:15:23 -0700

    update for file IO

--- Ex_Files_C_EssT/Exercise_Files/13_01.c
+++ Ex_Files_C_EssT/Exercise_Files/13_01.c
@@ -19,9 +19,9 @@ void writeToFile(void);
 int main(void) {
 
 	readEntireFile();
-	//printf("\n--------------------------------\n");
-	//readUntilMarker('m');
-	//writeToFile();
+	printf("\n--------------------------------\n");
+	readUntilMarker('m');
+	writeToFile();
 
 	return EXIT_SUCCESS;
 }
@@ -29,7 +29,7 @@ int main(void) {
 void readEntireFile(){
 	int ch;
 	FILE *fp;  // pointer to a file type
-	fp = fopen("C:\Github\C_Study\Ex_Files_C_EssT\Exercise_Files\message1301", "r"); // Change to match your path
+	fp = fopen("C:/Github/C_Study/Ex_Files_C_EssT/Exercise_Files/message1301", "r"); // Change to match your path
 	ch = getc(fp);
 	while (ch != EOF){  // keep looping until End Of File
 		putchar(ch);    // print the characters read
@@ -41,7 +41,7 @@ void readEntireFile(){
 void readUntilMarker(char marker){
 	int ch;
 	FILE *fp;  // pointer to a file type
-	fp = fopen("C:\Github\C_Study\Ex_Files_C_EssT\Exercise_Files\message1301", "r"); // Change to match your path
+	fp = fopen("C:/Github/C_Study/Ex_Files_C_EssT/Exercise_Files/message1301", "r"); // Change to match your path
 	ch = getc(fp);
 	while (ch != marker){  // keep looping until End Of File
 		putchar(ch);       // print the characters read
@@ -52,7 +52,7 @@ void readUntilMarker(char marker){
 
 void writeToFile(){
 	FILE *fp;  // pointer to a file type
-	fp = fopen("C:\Github\C_Study\Ex_Files_C_EssT\Exercise_Files\message2", "w"); // Change to match your path
+	fp = fopen("C:/Github/C_Study/Ex_Files_C_EssT/Exercise_Files/message2", "w"); // Change to match your path
 	char name[10] = "Roger";
 	fprintf(fp,"This message is for %s\n",name);
 	fclose(fp);
